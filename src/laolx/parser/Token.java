c56d7707a61a76df28b451e5aebf7e833c3ed3b7
Karl W. Pfalzer <kwpfalzer@sbcglobal.net>
2017-09-15 14:30:16 -0700

    beef up Token to separate symbols and keywords

--- src/laolx/parser/Token.java
+++ src/laolx/parser/Token.java
@@ -23,21 +23,54 @@
  */
 package laolx.parser;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 /**
  * A token.
  *
  * @author kwpfalzer
  */
 public class Token {
+
     public Token(Location location, Code code, String text) {
         this.location = location;
         this.code = code;
         this.text = text;
     }
-    
+
     public final Location location;
     public final Code code;
     public final String text;
+
+    /**
+     * Check if text matches a keyword.
+     * @param text text to check.
+     * @return Code of matching keyword or null if not a keyword.
+     */
+    public static Code isKeyword(String text) {
+        return KEYWORDS.get(text);
+    }
+    
+    /**
+     * Check is text matches a keyword.
+     * @param text text to check.
+     * @param ref set to keyword code iff text is a keyword; else do nothing.
+     * @return true if text matches keyword (and ref[0] updated); else false;
+     */
+    public static boolean isKeyword(String text, Code ref[]) {
+        final Code kwrd = isKeyword(text);
+        if (Objects.nonNull(kwrd)) {
+            assert 0 < ref.length;
+            ref[0] = kwrd;
+        }
+        return Objects.nonNull(kwrd);
+    }
     
     /**
      * Token codes.
@@ -54,70 +87,143 @@ public class Token {
         SYMBOL, //e.g. :foobar
         //
         // K_xxx are keywords
-        K_CLASS,
-        K_INTERFACE,
-        K_EXTENDS,
-        K_IMPLEMENTS,
-        K_DEFAULT,
-        K_STATIC,
-        K_FOR,
-        K_WHILE,
-        K_UNTIL,
-        K_CASE,
-        K_WHEN,
-        K_IF,
-        K_ELSE,
-        K_ELSIF,
-        K_THROW,
-        K_CATCH,
-        K_PUBLIC,
-        K_PROTECTED,
-        K_PRIVATE,
-        K_NIL,
-        K_INT,
-        K_FLOAT,
-        K_STRING,
+        K_CLASS("class"),
+        K_INTERFACE("interface"),
+        K_EXTENDS("extends"),
+        K_IMPLEMENTS("implements"),
+        K_DEFAULT("default"),
+        K_STATIC("static"),
+        K_FOR("for"),
+        K_WHILE("while"),
+        K_UNTIL("until"),
+        K_CASE("case"),
+        K_WHEN("when"),
+        K_IF("if"),
+        K_ELSE("else"),
+        K_ELSIF("elsif"),
+        K_THROW("throw"),
+        K_CATCH("catch"),
+        K_PUBLIC("public"),
+        K_PROTECTED("protected"),
+        K_PRIVATE("private"),
+        K_NIL("nil"),
+        K_INT("int"),
+        K_FLOAT("float"),
+        K_STRING("string"),
         //
         // S_xxx are symbols
-        S_TIDLE, //~
-        S_EXCLAMATION, //!
-        S_AT, //@
-        S_POUND, //#
-        S_DOLLAR, //$
-        S_PCNT, //%
-        S_CARET, //^
-        S_AND, //&
-        S_AND2, //&&
-        S_STAR, //*
-        S_STAR2, //**
-        S_LPAREN, //(
-        S_RPAREN, //)
-        S_MINUS, //-
-        S_PLUS, //+
-        S_PLUS2, //++
-        S_EQ, //=
-        S_PLUSEQ, //+=
-        S_MINUSEQ, //-=
-        S_LCURLY, //{
-        S_RCURLY, //}
-        S_LBRACK, //[
-        S_RBRACK, //]
-        S_OR, //|
-        S_OR2, //||
-        S_BSLASH, //\
-        S_COLON, //:
-        S_COLON2, //::
-        S_SEMI, //;
-        S_DQOUTE, //"
-        S_SQUOTE, //'
-        S_LT, //<
-        S_LT2, //<<
-        S_GT, //>
-        S_GT2, //>>
-        S_COMMA, //,
-        S_DOT, //.
-        S_QMARK, //?
-        S_DIV, // /
-        S_BACKTIC, //`
+        S_TIDLE("~"),
+        S_EXCLAMATION("!"),
+        S_AT("@"),
+        S_POUND("#"),
+        S_DOLLAR("$"),
+        S_PCNT("%"),
+        S_CARET("^"),
+        S_AND("&"),
+        S_ANDEQ("&="),
+        S_AND2("&&"),
+        S_AND2EQ("&&="),
+        S_STAR("*"),
+        S_STAREQ("*="),
+        S_STAR2("**"),
+        S_STAR2EQ("**="),
+        S_LPAREN("("),
+        S_RPAREN(")"),
+        S_MINUS("-"),
+        S_PLUS("+"),
+        S_PLUS2("++"),
+        S_EQ("="),
+        S_PLUSEQ("+="),
+        S_MINUSEQ("-="),
+        S_LCURLY("{"),
+        S_RCURLY("}"),
+        S_LBRACK("["),
+        S_RBRACK("]"),
+        S_OR("|"),
+        S_OREQ("|="),
+        S_OR2("||"),
+        S_OR2EQ("||="),
+        S_BSLASH("\\"),
+        S_COLON(":"),
+        S_COLON2("::"),
+        S_SEMI(";"),
+        S_DQOUTE("\""),
+        S_SQUOTE("'"),
+        S_LT("<"),
+        S_LTEQ("<="),
+        S_LT2("<<"),
+        S_LT2EQ("<<="),
+        S_GT(">"),
+        S_GTEQ(">="),
+        S_GT2(">>"),
+        S_GT2EQ(">>="),
+        S_COMMA(","),
+        S_DOT("."),
+        S_QMARK("?"),
+        S_DIV("/"),
+        S_BACKTIC(",");
+
+        private Code() {
+            this(null);
+        }
+
+        private Code(String text) {
+            this.text = text;
+        }
+
+        final String text;
     }
+
+    public static final Map<String, Code> KEYWORDS = new HashMap<>();
+    public static final Map<Character, Code[]> SYMBOLS = new HashMap<>();
+
+    static {
+        initKeywords();
+        initSymbols();
+    }
+
+    /**
+     * Initialize KEYWORDS to Code by String/keyword.
+     */
+    private static void initKeywords() {
+        Stream.of(Code.class.getEnumConstants())
+                .filter(code -> code.toString().startsWith("K_"))
+                .forEach((Code code) -> {
+                    KEYWORDS.put(code.text, code);
+                });
+    }
+
+    /**
+     * Initialize SYMBOLS to array Code[] by 1st symbol char. Code[] is sorted
+     * longest to shortest *by text.length); i.e., to find longest matching.
+     */
+    private static void initSymbols() {
+        Map<Character, List<Code>> codesByChar = new HashMap<>();
+        Stream.of(Code.class.getEnumConstants())
+                .filter(code -> code.toString().startsWith("S_"))
+                .forEach((Code code) -> {
+                    final Character first = code.text.charAt(0);
+                    if (!codesByChar.containsKey(first)) {
+                        codesByChar.put(first, new LinkedList<>());
+                    }
+                    codesByChar.get(first).add(code);
+                });
+        codesByChar.entrySet().stream().forEach(e -> {
+            Code[] value = e
+                    .getValue()
+                    .stream()
+                    .sorted(CODE_COMPARATOR)
+                    .toArray(Code[]::new);
+            SYMBOLS.put(e.getKey(), value);
+        });
+    }
+
+    /**
+     * Lambda for descending order (by text.length).
+     */
+    private static final Comparator<Code> CODE_COMPARATOR = (Code o1, Code o2) -> {
+        final int len1 = o1.text.length(), len2 = o2.text.length();
+        return (len1 < len2) ? 1 : ((len1 == len2) ? 0 : -1);
+    };
+
 }
